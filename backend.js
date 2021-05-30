3a1f9ff851c5c47dca0b81cdf44e767435e99109
eitstar <eitanmarks25@gmail.com>
2021-05-30 03:19:22 +0300

    d3

--- backend.js
+++ backend.js
@@ -1,9 +1,10 @@
-const pg = require('pg');
 const exp = require('express');
 const bp = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
-// const { Client } = require('pg');
+const Client = require('pg').Client
+    // const path = require('path');
+    // const filePath = path.join(__dirname, '/public');;
 const app = exp();
 app.use(exp.json());
 
@@ -14,36 +15,114 @@ app.use(bp.json())
 const urlencodedParser = bp.urlencoded({ extended: false })
 app.use(cors());
 
-const cs = 'postgres://postgres:password369@localhost:5432/virtualride';
-const client = new pg.Client(cs);
-
-// const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName";
-
-app.get('/homepage', (req, res) => {
-    client.connect()
-        .then(() => console.log("Connected successfully"))
-        .then(() => client.query("select country_name from countries"))
-        .then(results => console.table(results.rows))
-        .then(() => client.query("select continent from currencies"))
-        .then(results => console.table(results.rows))
-        .catch(e => console.log(e))
-        .finally(() => client.end())
-    res.setHeader('Content-Type', 'text/plain')
-
-    res.send(results => JSON.stringify(results.rows))
+const client = new Client({
+    user: 'postgres',
+    password: 'password369',
+    host: 'localhost',
+    port: '5432',
+    database: "virtualride"
 
 })
 
+
+app.use('/static', exp.static('/.public'))
+    // fs.openSync(filePath, '/.public');
+    // console.log(__dirname)
+
+app.get('/homepage', (req, res) => {
+
+    res.sendFile('public/index.html', { root: __dirname })
+    console.log('in homepage get...')
+    execute()
+        .then(result => {
+            console.log('hello from then')
+            console.table(result.rows)
+            res.send(result.rows)
+        })
+})
 
 app.post('/homepage', urlencodedParser, (req, res) => {
     console.log('post in homepage...')
-    console.log(req.body)
-    execute(req.body)
-    res.send(req.body.curResults)
+        // console.log(req.body)
+        // execute(req.body)
+        // res.send(req.body.curResults)
 
 })
 
-app.listen(5555, () => console.log('Example app listening on port 5555!'));
-// app.route('/homepage', (req, res) => {
-//     res.send('get in route...')
-// })
+async function execute() {
+    try {
+        await client.connect()
+        console.log("Connected successfully")
+        const couResults = await client.query("select country_name from countries")
+        return couResults
+    } catch (err) {
+        console.log(`Somthing want worng ${err}`)
+    } finally {
+        await client.end()
+        console.log("client disconnected successfully")
+    }
+}
+
+
+
+/// all countreis
+
+
+
+app.route('/australia')
+    .get((res, req) => {
+        res.sendFile('public/australia.html', { root: __dirname })
+        console.log('in get australia ')
+
+
+    })
+    .post((res, req) => {
+        console.log('in post australia ')
+
+    })
+
+app.route('/france')
+    .get((res, req) => {
+        res.sendFile('public/france.html', { root: __dirname })
+        console.log('in get france ')
+    })
+    .post((res, req) => {
+        console.log('in post france ')
+
+    })
+
+app.route('/brazil')
+    .get((res, req) => {
+        res.sendFile('public/brazil.html', { root: __dirname })
+        console.log('in get brazil ')
+    })
+    .post((res, req) => {
+        console.log('in post brazil ')
+
+    })
+
+app.route('/china')
+    .get((res, req) => {
+        res.sendFile('public/china.html', { root: __dirname })
+        console.log('in get china ')
+    })
+    .post((res, req) => {
+        console.log('in post china ')
+
+    })
+
+app.route('/egypt')
+    .get((res, req) => {
+        res.sendFile('public/egypt.html', { root: __dirname })
+        console.log('in get egypt ')
+    })
+    .post((res, req) => {
+        console.log('in post egypt ')
+
+    })
+
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- public/script.js
+++ public/script.js
@@ -1,55 +1,42 @@
-// const formTag = document.getElementById('formID').submit(function(e) {
-//     e.preventDefault();
-// });
+const getCountriesDataFromDb = (async() => {
+
+    const data = await fetch('http://localhost:3000/homepage')
+    const result = await data.json()
+    return await putAllDatainSecstion(result)
+
+})()
+
+const putAllDatainSecstion = async(data) => {
+    const country = document.querySelector('select')
+    console.log(country)
+    let counter = 0
+    for (let item of data) {
+        if (counter === 5)
+            return
+        counter++
+        let option = document.createElement('option')
+        option.innerHTML += item.country_name
+        country.appendChild(option)
+
+    }
 
 
 
-const sendOption = () => {
-    let continent = document.querySelector('#continent').value
-    let country = document.querySelector('#country').value
+}
 
-
-    fetch('http://localhost:5555/', {
-        method: "get",
-        headers: {
-            'Content-Type': 'application/json'
+const findOptionVal = () => {
+    const options = document.querySelectorAll('option')
+    let optionVal;
+    options.forEach(item => {
+        if (item.selected) {
+            return optionVal = item.value.toLocaleLowerCase()
         }
-
-    }).then(function(res) {
-        res.text().then(function(text) {
-            poemDisplay.textContent = text;
-
-        });
-    });
+    })
+    return goToRoute(optionVal)
 
 }
 
 
-
-// const filter = async() => {
-//     try {
-//         deleteAll()
-
-//         // const selectcontinent = document.querySelector('#continent').value.toLowerCase()
-//         // const selectcountry = document.querySelector('#country').value.toLowerCase()
-
-
-//     } catch (err) {
-
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-// const selectcontinent = document.querySelector('#continent')
-// const selectcountry = document.querySelector('#country')
-
-// selectcontinent.addEventListener('change', filter)
-// selectcountry.addEventListener('change', filter)
+const goToRoute = async(optionVal) => {
+    await fetch(`http://localhost:3000/${optionVal}`)
+}
